604eaa122229ae8bd1d1b8540b4461f0a36f0ee9
Matt Egbers <matt.egbers@contractor.cengage.com>
2013-03-22 21:01:40 -0400

    Added the ability to remove elements by draging to the top left corned.  Doesnt really work yet.  Need to figure out how to force redraw and actually have it redraw stuff

--- my_checmical_romance.js
+++ my_checmical_romance.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+    //Why doesnt this stay on the Canvas
+    $("canvas").drawRect({
+        fillStyle: "#000",
+        x: 0,
+        y: 0,
+        width: 50,
+        height: 50,
+        fromCenter: false
+    });
+
     $.jCanvas.extend({
         name: "drawChemicalElement",
         props: {},
@@ -17,18 +27,16 @@ $(document).ready(function(){
                 shadowBlur: 10,
                 fromCenter: false
             })
+            .drawText({
+                fillStyle: "#9cf",
+                strokeStyle: "#25a",
+                strokeWidth: 2,
+                x: params.x + 25,
+                y: params.y + 25,
+                font: "36pt Verdana, sans-serif",
+                text: params.symbol
 
-                .drawText({
-                    fillStyle: "#9cf",
-                    strokeStyle: "#25a",
-                    strokeWidth: 2,
-                    x: params.x + 25,
-                    y: params.y + 25,
-                    font: "36pt Verdana, sans-serif",
-                    text: params.symbol
-
-                })
-            ;
+            });
 
             $.jCanvas.detectEvents(this, ctx, params);
 
@@ -48,3 +56,28 @@ $(document).ready(function(){
 
 
 });
+
+var mixingBoard = {
+    chemicals: [],
+
+    addChemical: function(chemical) {
+        this.chemicals.push(chemical);
+    },
+
+    removeChemical: function(chemical) {
+        for(var i = 0; i < this.chemicals.length; i++) {
+            if(this.chemicals[i].id === chemical.id) {
+                $("canvas").removeLayer(this.chemicals[i].id);
+                this.chemicals.splice(i, 1);
+            }
+        }
+    },
+
+    updateChemical: function(chemical) {
+        for(var i = 0; i < this.chemicals.length; i++) {
+            if(this.chemicals[i].id === chemical.id) {
+                this.chemicals[i] = chemical;
+            }
+        }
+    }
+};
