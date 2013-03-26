e74e7a39e2e87ef394849629c734310671f6d2c2
dave & abdul <abdul.habra@contractor.cengage.com>
2013-03-26 13:04:08 -0400

    added _, tests, and started add()

--- public/js/mcr.backend.js
+++ public/js/mcr.backend.js
@@ -1,15 +1,64 @@
-(function (root, $) {
+(function (root, $, _) {
     'use strict';
 
     var compounds, discoveries, elements, ready = $.Deferred();
+    var workspace= [];
 
     var mcr = root.mcr = {
+
         add: function (symbol) {
+            workspace.push(symbol);
+
+            var discovered = findMatchedCompounds();
+            return {
+                workspace: workspace,
+                discovered: discovered,
+                potential: 0
+            };
+        },
+        remove: function(symbol) {
             return {};
         },
+
+        reset: function() {
+            workspace = [];
+        },
+
         ready: ready,
-        symbols: {}
+        symbols: {},
+        load: load,
+        docRoot: ''
     };
+
+    function findMatchedCompounds() {
+        var map = parseWorkspace();
+        var result = [];
+
+        for (var i= 0, n=compounds.length; i<n; i++) {
+            if (isMatch(map, compounds[i])) {
+                result.push(compounds[i]);
+            }
+        }
+
+        return result;
+    }
+
+    function isMatch(map, compound) {
+        return _.isEqual(map, compound.elements);
+    }
+
+    function parseWorkspace() {
+        var map = {};
+        for (var i= 0, n=workspace.length; i<n; i++) {
+            var c= workspace[i];
+            if (map[c]) {
+                map[c] = map[c] + 1;
+            } else {
+                map[c] = 1;
+            }
+        }
+        return map;
+    }
 
     function initCompounds(data) {
         compounds = data;
@@ -34,12 +83,13 @@
         });
     }
 
-    $.when($.ajax('json/compounds.json'), $.ajax('json/elements.json')).then(function (data1, data2) {
-        initCompounds(data1[0]);
-        initElements(data2[0].PERIODIC_TABLE.ATOM);
-        ready.resolve();
-    }, function () {
-        console.log('error');
-    });
-
-})(this, this.jQuery);
+    function load() {
+        $.when($.ajax(mcr.docRoot + 'json/compounds.json'), $.ajax(mcr.docRoot + 'json/elements.json')).then(function (data1, data2) {
+            initCompounds(data1[0]);
+            initElements(data2[0].PERIODIC_TABLE.ATOM);
+            ready.resolve();
+        }, function () {
+            console.log('error');
+        });
+    }
+})(this, this.jQuery, this._);
--- test/mcr.backend_spec.js
+++ test/mcr.backend_spec.js
@@ -1,7 +1,57 @@
-/**
- * Created with JetBrains WebStorm.
- * User: mindtap
- * Date: 3/26/13
- * Time: 10:33 AM
- * To change this template use File | Settings | File Templates.
- */
+mcr.docRoot = 'public/';
+mcr.load();
+
+describe('mcr.backend', function () {
+
+    beforeEach(function() {
+        mcr.reset();
+        var done = false;
+
+        runs(function(){
+            mcr.ready.done(function() {
+                done = true;
+            });
+        });
+
+        waitsFor(function(){
+            return done;
+        });
+
+    });
+
+    describe('add', function () {
+        it('maintains workspace', function () {
+            var result = mcr.add('H');
+            expect(result.workspace).toEqual(['H']);
+            result = mcr.add('H');
+            expect(result.workspace).toEqual(['H', 'H']);
+        });
+
+        it('finds water when given hho', function () {
+            mcr.add('H');
+            mcr.add('H');
+            var result = mcr.add('O');
+
+            var expected = [
+                {
+                    "name":"Water",
+                    "group": "covalent",
+                    "elements":{
+                        "H":2,
+                        "O":1
+                    }
+                }
+            ];
+            expect(result.discovered).toEqual(expected);
+        });
+
+        it('finds nothing when no match', function () {
+            mcr.add('H');
+            var result = mcr.add('O');
+
+            expect(result.discovered).toEqual([]);
+            //expect(result.potential).toEqual(1);
+        });
+    });
+});
+
