fa03a1c57cd8978306edd4f80aaacdecb4d235a7
dmichon <dmichon@ubuntu.(none)>
2013-04-08 13:18:05 -0400

    added formatting of boxes around chemical formulas

--- public/js/mcr.frontend.js
+++ public/js/mcr.frontend.js
@@ -53,26 +53,39 @@ $(document).ready(function(){
         props: {},
         fn: function(ctx, params) {
 
-            $("canvas").drawRect({
+            $('canvas')
+                .drawText({
+                    layer: 'myLayer',
+                    name: 'myText',
+                    fillStyle: "#36c",
+                    strokeStyle: "#25a",
+                    strokeWidth: 2,
+                    x: -100, y: -100,
+                    font: "36pt Verdana, sans-serif",
+                    text: params.symbol
+                });
+            var symbolWidth = $('canvas').measureText('myText').width;
+            //$("canvas").removeLayer('myLayer');
+
+            $('canvas')
+            .drawRect({
                 fillStyle: params.fillStyle,
-                x: params.x,
-                y: params.y,
-                width: params.width,
-                height: params.height,
-                shadowColor: "#000",
-                shadowBlur: 10,
-                fromCenter: false
-            })
-            .drawText({
-                fillStyle: "#9cf",
+                strokeStyle: '#000',
+                strokeWidth: 1,
+                x: params.x, y: params.y,
+                width: symbolWidth+10,
+                height: params.height
+            }).drawText({
+                name: 'myText',
+                fillStyle: "#36c",
                 strokeStyle: "#25a",
                 strokeWidth: 2,
-                x: params.x + 25,
-                y: params.y + 25,
-                font: "28pt Verdana, sans-serif",
+                x: params.x, y: params.y,
+                font: "36pt Verdana, sans-serif",
                 text: params.symbol
-
             });
+
+
 
             $.jCanvas.detectEvents(this, ctx, params);
         }
