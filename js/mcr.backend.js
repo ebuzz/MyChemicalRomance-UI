ab5776a64bee9fdeaf1b3d42927580ced7cc0e07
Matt Egbers <matt.egbers@contractor.cengage.com>
2013-04-08 16:57:38 -0400

    Posting results to the git page

--- js/mcr.backend.js
+++ js/mcr.backend.js
@@ -15,8 +15,8 @@
             }
             return result;
         },
-        undiscoveredCompounds: function() {
-            return findPotentialCompounds().length;
+        undiscoveredCompounds: function(symbol) {
+            return findPotentialCompounds(symbol).length;
         },
         workspace: function() {
             return workspace;
@@ -72,8 +72,14 @@
         };
     }
 
-    function findPotentialCompounds() {
-        var map = parseWorkspace();
+    function findPotentialCompounds(symbol) {
+
+        var map = {};
+        if (symbol === undefined) {
+            map = parseWorkspace();
+        } else {
+            map[symbol] = 1;
+        }
         var result = [];
 
         for (var i= 0, n=compounds.length; i<n; i++) {
@@ -140,7 +146,7 @@
     }
 
     function load() {
-        $.when($.ajax(mcr.compoundsUrl), $.ajax(mcr.elementsUrl)).then(function (data1, data2) {
+        $.when($.ajax({url:mcr.compoundsUrl,dataType:'json'}), $.ajax({url:mcr.elementsUrl,dataType:'json'})).then(function (data1, data2) {
             initCompounds(data1[0]);
             initElements(data2[0].PERIODIC_TABLE.ATOM);
             ready.resolve();
--- js/mcr.frontend.js
+++ js/mcr.frontend.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
     mcr.ready.done(function() {
     'use strict';
+    drawElements();
     //------Game Loop-----------
     var ONE_FRAME_TIME = 1000 / 60;
     var mainLoop = function() {
@@ -27,7 +28,17 @@ $(document).ready(function(){
         }
     }
 
+    function renderCompounds(tabSelector, compounds) {
+        $(tabSelector).empty();
+        for(var i = 0; i < compounds.length; i++) {
+            $(tabSelector).append('<div>' +transformNumbers(compounds[i].formula) + ' - ' + compounds[i].name + '</div>');
+        }
+        $(tabSelector + "-count").empty();
+        $(tabSelector + "-count").append(compounds.length);
+    }
+
     $( "#tabs" ).tabs();
+
     var searchDialog = $( "#searchDialog" ).dialog({
         autoOpen: false,
         draggable: false,
@@ -43,26 +54,39 @@ $(document).ready(function(){
         props: {},
         fn: function(ctx, params) {
 
-            $("canvas").drawRect({
+            $('canvas')
+                .drawText({
+                    layer: 'myLayer',
+                    name: 'myText',
+                    fillStyle: "#36c",
+                    strokeStyle: "#25a",
+                    strokeWidth: 2,
+                    x: -100, y: -100,
+                    font: "36pt Verdana, sans-serif",
+                    text: params.symbol
+                });
+            var symbolWidth = $('canvas').measureText('myText').width;
+            //$("canvas").removeLayer('myLayer');
+
+            $('canvas')
+            .drawRect({
                 fillStyle: params.fillStyle,
-                x: params.x,
-                y: params.y,
-                width: params.width,
-                height: params.height,
-                shadowColor: "#000",
-                shadowBlur: 10,
-                fromCenter: false
-            })
-            .drawText({
-                fillStyle: "#9cf",
+                strokeStyle: '#000',
+                strokeWidth: 1,
+                x: params.x, y: params.y,
+                width: symbolWidth+10,
+                height: params.height
+            }).drawText({
+                name: 'myText',
+                fillStyle: "#36c",
                 strokeStyle: "#25a",
                 strokeWidth: 2,
-                x: params.x + 25,
-                y: params.y + 25,
-                font: "28pt Verdana, sans-serif",
+                x: params.x, y: params.y,
+                font: "36pt Verdana, sans-serif",
                 text: params.symbol
-
             });
+
+
 
             $.jCanvas.detectEvents(this, ctx, params);
         }
@@ -84,29 +108,39 @@ $(document).ready(function(){
         $("canvas").drawImage({
             name:'controls',
             layer: true,
+            source: "images/reactor_area.jpg",
+            x: 675, y: 200,
+            scale: 0.6
+        });
+
+        $("canvas").drawImage({
+            name:'controls',
+            layer: true,
             source: "images/trashcan_full.png",
             x: 875, y: 375,
             scale: 0.3
         });
 
-        $("canvas").drawImage({
-            name:'controls',
-            layer: true,
-            source: "images/search.png",
-            x: 455, y: 375,
-            scale: 0.5,
-            click : function(layer) {
-                $("#searchDialog").dialog('open');
-            }
-        });
+//        $("canvas").drawImage({
+//            name:'controls',
+//            layer: true,
+//            source: "images/search.png",
+//            x: 455, y: 375,
+//            scale: 0.5,
+//            click : function(layer) {
+//                $("#searchDialog").dialog('open');
+//            }
+//        });
 
-        $("canvas").drawImage({
-            name:'controls',
-            layer: true,
-            source: "images/help.png",
-            x: 25, y: 375,
-            scale: 0.3
-        });
+//        $("canvas").drawImage({
+//            name:'controls',
+//            layer: true,
+//            source: "images/help.png",
+//            x: 25, y: 375,
+//            scale: 0.3
+//        });
+
+
 
         drawPotentialCount(mcr.undiscoveredCompounds());
 
@@ -126,11 +160,21 @@ $(document).ready(function(){
 
     //Attach Events to Table
     $(".symbol").each(function(i, periodicElement) {
+        var symbol = $(this).find("abbr").text();
         $(periodicElement).on('click', function(event) {
-            var symbol = $(this).find("abbr").text();
             addElementToCanvas(symbol);
         });
     });
+
+    function drawElements() {
+        $(".symbol").each(function(i, periodicElement) {
+            var symbol = $(this).find("abbr").text();
+            if (mcr.undiscoveredCompounds(symbol) === 0) {
+                debugger;
+                $(this).find("abbr").addClass('noPotential');
+            }
+        });
+    }
 
     $('#chemSymbol').on('change', function() {
         addElementToCanvas();
@@ -143,43 +187,123 @@ $(document).ready(function(){
     function addElementToCanvas(addedSymbol) {
         var symbol = addedSymbol ? addedSymbol : $('#chemSymbol').val();
 
+        debugger;
         if(symbol === '') {
             return;
         }
 
-        var result = mcr.add(symbol);
-        var x = Math.floor((Math.random()*700)+100);
+        var x = Math.floor((Math.random()*700)+100) / 2;
         var y = Math.floor((Math.random()*200)+100);
-        if(result.discovered.length > 0) {
-            var chemical = {
-                id: currentId++,
-                symbol: result.discovered[0].formula,
-                x: x,
-                y: y
-            };
-            mixingBoard.removeAll();
-            //animateExplosion();
-            resetUI();
-            mixingBoard.addChemical(chemical);
-            drawChemical(chemical, x, y);
-            mcr.add(chemical.symbol);
-            window.animateExplosion(x, y);
-        }else {
-            var chemical = {
-                id: currentId++,
-                symbol: symbol,
-                x: x,
-                y: y
-            };
-            mixingBoard.addChemical(chemical);
-            drawChemical(chemical,x,y);
-        }
-        drawPotentialCount(result.potential);
-
+        var chemical = {
+            id: currentId++,
+            symbol: symbol,
+            x: x,
+            y: y
+        };
+        mixingBoard.addChemical(chemical);
+        drawChemical(chemical, x, y);
     }
 
+        function transformNumbers(symbol) {
+            var numbersRegex = RegExp("[\-\+0-9]+");
+            var lettersRegex = RegExp("[A-z]+");
+
+            var symbols = symbol.split(numbersRegex);
+            var numbers = symbol.split(lettersRegex);
+
+            if (numbers[0] == '') {
+                for (var i=1; i<numbers.length; i++) {
+                    numbers[i-1] = convertToSubOrSup(numbers[i]);
+                }
+                numbers[numbers.length-1] = "";
+            } else {
+                for (var i=0; i<numbers.length; i++) {
+                    numbers[i] = convertToSubOrSup(numbers[i]);
+                }
+            }
+
+
+            var convertedSymbol = "";
+            for (var i=0; i<symbols.length; i++) {
+                debugger;
+                convertedSymbol += symbols[i];
+                if (numbers[i] !== undefined) {
+                    convertedSymbol += numbers[i];
+                }
+            }
+            return convertedSymbol;
+        }
+
+        function convertToSubOrSup(number) {
+            var convertedNumber = "";
+            var numberIsSuper = false;
+            for (var i=0; i < number.length; i++) {
+                switch(number[i])
+                {
+                    case '1':
+                        convertedNumber += numberIsSuper ? "\u2071" : "\u2081";
+                        numberIsSuper = false;
+                        break;
+                    case '2':
+                        convertedNumber += numberIsSuper ? "\u00B2" : "\u2082";
+                        numberIsSuper = false;
+                        break;
+                    case '3':
+                        convertedNumber += numberIsSuper ? "\u00B3" : "\u2083";
+                        numberIsSuper = false;
+                        break;
+                    case '4':
+                        convertedNumber += numberIsSuper ? "\u2074" : "\u2084";
+                        numberIsSuper = false;
+                        break;
+                    case '5':
+                        convertedNumber += numberIsSuper ? "\u2075" : "\u2085";
+                        numberIsSuper = false;
+                        break;
+                    case '6':
+                        convertedNumber += numberIsSuper ? "\u2076" : "\u2086";
+                        numberIsSuper = false;
+                        break;
+                    case '7':
+                        convertedNumber += numberIsSuper ? "\u2077" : "\u2087";;
+                        numberIsSuper = false;
+                        break;
+                    case '8':
+                        convertedNumber += numberIsSuper ? "\u2078" : "\u2088";
+                        numberIsSuper = false;
+                        break;
+                    case '9':
+                        convertedNumber += numberIsSuper ? "\u2079" : "\u2089";
+                        numberIsSuper = false;
+                        break;
+                    case '0':
+                        convertedNumber += numberIsSuper ? "\u2070" : "\u2080";
+                        numberIsSuper = false;
+                        break;
+                    case '+':
+                        numberIsSuper = true;
+                        convertedNumber += numberIsSuper ? "\u207A" : "\u208A";
+                        break;
+                    case '-':
+                        numberIsSuper = true;
+                        convertedNumber += numberIsSuper ? "\u207B" : "\u208B";
+                        break;
+                }
+            }
+            var constructedNumber = "";
+            for (var i=0; i < convertedNumber.length; i++) {
+                if (i != (convertedNumber.length-1) && (convertedNumber[i] === "\u207B" || convertedNumber[i] === "\u207A")) {
+                    constructedNumber += convertedNumber[i+1] + convertedNumber[i];
+                    i++;
+                } else {
+                    constructedNumber += convertedNumber[i];
+                }
+            }
+
+            return constructedNumber;
+        }
+
     function drawChemical(chemical,x,y) {
-        //mixingBoard.addChemical(chemical);
         var x = x !==undefined? x : Math.floor((Math.random()*700)+100);
         var y = y!==undefined? y : Math.floor((Math.random()*200)+100);
         $("canvas").drawChemicalElement({
@@ -188,18 +312,52 @@ $(document).ready(function(){
             layer: true,
             draggable: true,
             fillStyle: "#fff",
-            symbol: chemical.symbol,
+            symbol: transformNumbers(chemical.symbol),
             width: 50,
             height: 50,
             x: x,
             y: y,
             dragstop: function(event) {
                 suspendRedraw = false;
-                var withinXBoundary = (event.x > 810);
-                var withinYBoundary = (event.y > 315);
-                if(withinXBoundary && withinYBoundary) {
+
+                if((event.x > 810) && (event.y > 315)) {
                     mixingBoard.removeChemical(chemical);
                     drawPotentialCount(mcr.undiscoveredCompounds());
+                } else if(event.x >= 450) {
+                    var result = mcr.add(chemical.symbol);
+                    if(result.discovered.length > 0) {
+                        var x = 750;
+                        var y = 200;
+                        var group = result.discovered[0].group;
+                        var tabSelect = "";
+                        if (group === "covalent") {
+                            tabSelect = "#tabs-3";
+                        } else if (group === "ionic") {
+                            tabSelect = "#tabs-2";
+                        } else if (group === "cation") {
+                            tabSelect = "#tabs-4";
+                        } else if (group === "anion") {
+                            tabSelect = "#tabs-5";
+                        }
+                        renderCompounds(tabSelect,mcr.discoveredCompounds(group));
+                        mixingBoard.removeAllFromReactor();
+                        var foundChemical = {
+                            id: currentId++,
+                            symbol: result.discovered[0].formula,
+                            x: x,
+                            y: y
+                        };
+                        resetUI();
+                        mixingBoard.addChemical(foundChemical);
+                        drawChemical(foundChemical, x, y);
+                        mcr.add(foundChemical.symbol);
+                        window.animateExplosion(x, y);
+                        drawElements();
+                    }
+                    drawPotentialCount(result.potential);
+                } else if(event.x < 450){
+                    var result = mcr.remove(chemical.symbol);
+                    drawPotentialCount(result.potential);
                 }
             },
             mousedown: function(layer) {
@@ -221,6 +379,14 @@ var mixingBoard = {
 
     addChemical: function(chemical) {
         this.chemicals.push(chemical);
+    },
+
+    removeAllFromReactor: function() {
+        for(var i = this.chemicals.length-1; i >= 0; i--) {
+            if(this.chemicals[i].x > 450) {
+                this.removeChemical(this.chemicals[i]);
+            }
+        }
     },
 
     removeAll: function() {
